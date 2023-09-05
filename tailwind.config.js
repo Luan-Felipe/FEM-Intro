67232bc439c61aae1458fc2ce092616f4a44e4b6
Luan-Felipe <luanfel2009@gmail.com>
2023-09-05 14:57:41 -0300

    construção da navbar desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./**/*.{html,js}'],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            almostWhite: 'hsl(0, 0%, 98%)',
+            mediumGray: 'hsl(0, 0%, 41%)',
+            almostBlack: 'hsl(0, 0%, 8%)',
+        },
+        fontFamily: {
+            epilogue: ['Epilogue', 'sans-serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
